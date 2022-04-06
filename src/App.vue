557439d5858483aa81affdda211d435b9c43a94d
May Zin <scm.mayzinoo@gmail.com>
2022-04-06 17:39:00 +0630

    [add] vue todolist

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TaskList :tasks="tasks"></TaskList>
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+export default {
+  name: 'App',
+  components: {
+    TaskList
+  },
+  data () {
+    return {
+      tasks: [
+        {
+          title: 'Make todo list',
+          completed: true
+        },
+        {
+          title: 'Go skydiving',
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/TaskItem.vue
@@ -0,0 +1,31 @@
+<template id="task-item">
+    <li class="tasks__item">
+      <button :class="className"
+          @click.self="$emit('complete')"
+      >
+        {{ task.title }}
+      </button>
+      <button class="tasks__item__remove button alert pull-right"
+              @click="$emit('remove')"
+      >
+        <i class="fa fa-times"></i>
+      </button>
+    </li>
+</template>
+
+<script src="<https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js>"></script>
+<script>
+export default {
+  name: 'task-item',
+  props: ['task'],
+  computed: {
+    className() {
+      let classes = ['tasks__item__toggle'];
+      if (this.task.completed) {
+        classes.push('tasks__item__toggle--completed');
+      }
+      return classes.join(' ');
+    }
+  }
+}
+</script>
